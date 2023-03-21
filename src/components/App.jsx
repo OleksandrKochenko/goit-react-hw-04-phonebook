224dc4a1418efd906d95d5300ad42e6661e81174
OleksandrKochenko <121250212+OleksandrKochenko@users.noreply.github.com>
2023-03-22 00:01:40 +0200

    refactored form component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,71 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import shortid from 'shortid';
+import ContactForm from './form';
+import ContactList from './contact_list';
+import Filter from './filter';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  formSubmitHandler = data => {
+    const contactId = shortid.generate();
+    data.id = contactId;
+    if (
+      this.state.contacts.some(
+        contact => contact.name.toLowerCase() === data.name.toLowerCase()
+      )
+    ) {
+      alert(`${data.name} is already in contacts!`);
+    } else {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, data],
+      }));
+    }
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  handleDeleteBtn = e => {
+    const deletedId = e.currentTarget.name;
+    const deletedIndex = this.state.contacts.findIndex(
+      el => el.id === deletedId
+    );
+    this.setState(prevState => {
+      const prevContacts = prevState.contacts;
+      prevContacts.splice(deletedIndex, 1);
+      return {
+        contacts: [...prevContacts],
+      };
+    });
+  };
+
+  render() {
+    const filtredContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+    return (
+      <div
+        style={{
+          padding: '20px',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+
+        <h2>Contacts:</h2>
+        <Filter value={this.state.filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={filtredContacts}
+          handleClick={this.handleDeleteBtn}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
